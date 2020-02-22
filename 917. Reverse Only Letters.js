bc274114ba7b5a18fc38cbdfba66cbf51873c30f
jinho6225 <jinho6225@hotmail.com>
2020-02-22 14:09:02 -0800

    917. Reverse Only Letters

--- 917. Reverse Only Letters.js
+++ 917. Reverse Only Letters.js
@@ -2,7 +2,6 @@
 917. Reverse Only Letters
 Given a string S, return the "reversed" string where all characters
 that are not a letter stay in the same place, and all letters reverse their positions.
-
 Example 1:
 Input: "ab-cd"
 Output: "dc-ba"
@@ -12,7 +11,6 @@ Output: "j-Ih-gfE-dCba"
 Example 3:
 Input: "Test1ng-Leet=code-Q!"
 Output: "Qedo1ct-eeLg=ntse-T!"
-
 Note:
 S.length <= 100
 33 <= S[i].ASCIIcode <= 122
