362654c6c6a5e9571b1319207c6fdc17f306030f
jinho6225 <jinho6225@hotmail.com>
2020-02-22 13:59:17 -0800

    Reverse Only Letters 02222020

--- 917. Reverse Only Letters.js
+++ 917. Reverse Only Letters.js
@@ -18,3 +18,30 @@ S.length <= 100
 33 <= S[i].ASCIIcode <= 122
 S doesn't contain \ or "
 */
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var reverseOnlyLetters = function(str) {
+  var letterArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var array = Array.from(str)
+  var first = 0
+  var last = array.length-1
+  while (first < last) {
+    if (letterArr.includes(array[first])) {
+      if (letterArr.includes(array[last])) {
+        var temp = array[first]
+        array[first] = array[last]
+        array[last] = temp
+        first++
+        last--
+      } else {
+        last--
+      }
+    } else {
+      first++
+    }
+  }
+  return array.join('')
+}
