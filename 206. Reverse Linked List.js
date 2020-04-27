ad61abd5a8a5c83207ba59fa9131e2379b5d3b74
jinho6225 <jinho6225@hotmail.com>
2020-04-27 01:40:50 -0700

    206. reverse linked list - added recursive way

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -23,7 +23,24 @@ A linked list can be reversed either iteratively or recursively. Could you imple
  * @return {ListNode}
  */
 
+//Runtime: 56 ms, faster than 80.90% of JavaScript online submissions for Reverse Linked List.
+// recursively
+var reverseList = function (head) {
+  if (!head) {
+    return false;
+  }
+  if (head.next === null) {
+    return head;
+  }
+
+  let temp = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return temp;
+};
+
 //Runtime: 52 ms, faster than 93.69% of JavaScript online submissions for Reverse Linked List.
+//iteratively;
 var reverseList = function (head) {
   let prv = null;
   while (head) {
