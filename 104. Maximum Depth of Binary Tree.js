66f56b5ba8d54d3a2c3a247e3cf7b165688003ca
jinho6225 <jinho6225@hotmail.com>
2020-04-30 11:35:45 -0700

    104. maximun depth of binary tree

--- 104. Maximum Depth of Binary Tree.js
+++ 104. Maximum Depth of Binary Tree.js
@@ -1,6 +1,5 @@
 /*
 104. Maximum Depth of Binary Tree
-Easy
 
 Given a binary tree, find its maximum depth.
 The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
@@ -17,7 +16,7 @@ Given binary tree [3,9,20,null,null,15,7],
 return its depth = 3.
 */
 
-/**
+/*
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
