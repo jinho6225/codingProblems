b61bd230265ca9d363e4f7e5ff692fc74449b9a1
jinho6225 <jinho6225@hotmail.com>
2020-05-10 17:39:41 -0700

    1232. check if it is a straight line

---
+++ 1232. Check If It Is a Straight Line.js
@@ -0,0 +1,75 @@
+/*
+1232. Check If It Is a Straight Line
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+Example 1:
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+Example 2:
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+
+Constraints:
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+*/
+
+/*
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function (coordinates) {
+  let xIncrease = [];
+  let yIncrease = [];
+  for (let i = 1; i < coordinates.length; i++) {
+    if (coordinates[i][0] - coordinates[i - 1][0]) {
+      xIncrease.push(coordinates[i][0] - coordinates[i - 1][0]);
+    } else {
+      continue;
+    }
+    if (coordinates[i][1] - coordinates[i - 1][1]) {
+      yIncrease.push(coordinates[i][1] - coordinates[i - 1][1]);
+    } else {
+      continue;
+    }
+  }
+  if (xIncrease.length === 0 && yIncrease.length === 0) {
+    return false;
+  } else if (xIncrease.length === 0) {
+    for (let i = 0; i < yIncrease.length - 1; i++) {
+      if (yIncrease[i] !== yIncrease[i + 1]) {
+        return true;
+      }
+    }
+  } else if (yIncrease.length === 0) {
+    for (let i = 0; i < xIncrease.length - 1; i++) {
+      if (xIncrease[i] !== xIncrease[i + 1]) {
+        return true;
+      }
+    }
+  } else {
+    for (let i = 0; i < xIncrease.length - 1; i++) {
+      let xline = xIncrease[i + 1] - xIncrease[i];
+      let yline = yIncrease[i + 1] - yIncrease[i];
+      if (xline !== 0 || yline !== 0) {
+        if (yline % xline !== 0) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+// var coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]];
+// var coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+// var coordinates = [[-3,-2],[-1,-2],[2,-2],[-2,-2],[0,-2]]
+var coordinates = [
+  [0, 1],
+  [1, 3],
+  [-4, -7],
+  [5, 11],
+];
+checkStraightLine(coordinates);
