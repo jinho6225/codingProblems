9b19a6576ce54dee151a9fe726f121f92f416fa3
jinho6225 <jinho6225@hotmail.com>
2020-05-12 21:35:51 -0700

    21. merge two sorted list - faster & clean code

--- 21. Merge Two Sorted Lists.js
+++ 21. Merge Two Sorted Lists.js
@@ -22,11 +22,33 @@ Output: 1->1->2->3->4->4
  * @return {ListNode}
  */
 
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
+// Runtime: 60 ms, faster than 85.56% of JavaScript online submissions for Merge Two Sorted Lists.
+// Memory Usage: 35.6 MB, less than 61.54% of JavaScript online submissions for Merge Two Sorted Lists.
+var mergeTwoLists = function (l1, l2) {
+  let node = new ListNode();
+  let currentNode = node;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val <= l2.val) {
+      currentNode.next = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      currentNode.next = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    currentNode = currentNode.next;
+  }
+  if (l2 !== null) {
+    currentNode.next = l2;
+  }
+  if (l1 !== null) {
+    currentNode.next = l1;
+  }
+  node = node.next;
+  return node;
+};
 
+// Runtime: 72 ms, faster than 21.10% of JavaScript online submissions for Merge Two Sorted Lists.
+// Memory Usage: 36.5 MB, less than 5.13% of JavaScript online submissions for Merge Two Sorted Lists.
 var mergeTwoLists = function (l1, l2) {
   let arr = [];
   if (!l1 && !l2) return null;
