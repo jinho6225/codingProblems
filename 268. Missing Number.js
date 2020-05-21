e07829f15e3e40b295f936070c675b7a619d194a
jinho6225 <jinho6225@hotmail.com>
2020-05-21 15:58:39 -0700

    268.missing number

--- 268. Missing Number.js
+++ 268. Missing Number.js
@@ -18,6 +18,27 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
  * @return {number}
  */
 
+var missingNumber = function (nums) {
+  nums.sort((a, b) => a - b);
+  if (nums[0] !== 0) {
+    return 0;
+  }
+  let max = Math.max(...nums),
+    sum = 0,
+    total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  for (let i = 0; i <= max; i++) {
+    total += i;
+  }
+  if (sum === total) {
+    return max + 1;
+  } else {
+    return total - sum;
+  }
+};
+
 //super slow...
 var missingNumber = function (nums) {
   nums = nums.sort((a, b) => a - b);
