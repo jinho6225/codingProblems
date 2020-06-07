b1acc3f679ecd864c6de9b92f4c80ee3ff899eea
jinho6225 <jinho6225@hotmail.com>
2020-06-06 17:09:33 -0700

    14. longest common prefix - another solution added

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -20,6 +20,35 @@ All given inputs are in lowercase letters a-z.
  * @param {string[]} strs
  * @return {string}
  */
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return '';
+  if (strs.length === 1) return strs[0];
+  strs = strs.sort((a, b) => a.length - b.length);
+  let result = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    let isHas = false;
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== strs[0][i]) {
+        isHas = false;
+        break;
+      } else {
+        isHas = true;
+      }
+    }
+    if (isHas) {
+      result += strs[0][i];
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
 var longestCommonPrefix = function (strs) {
   if (strs.length === 0) {
     return '';
