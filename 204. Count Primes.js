58f5eb66091ccbba511e9441c1e31b5a7b1b2f2d
jinho6225 <jinho6225@hotmail.com>
2020-06-10 00:44:26 -0700

    204. count primes - another solution added

--- 204. Count Primes.js
+++ 204. Count Primes.js
@@ -13,6 +13,30 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
  * @param {number} n
  * @return {number}
  */
+
+const isPrime = (number) => {
+  if (number === 2) return true; // 2
+  if (number < 2 || !(number % 2)) {
+    //4,6,8
+    return false;
+  } //9 =>3
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    //3,5,7,
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+var countPrimes = function (n) {
+  let output = 0;
+  for (let i = 2; i < n; i++) {
+    //n10 //i=2,3,5,7 //i=4,6,8,9,10
+    if (isPrime(i)) {
+      output++; //1,1
+    }
+  }
+  return output;
+};
+
 var countPrimes = function (n) {
   if (n <= 2) return 0;
   let count = 0,
