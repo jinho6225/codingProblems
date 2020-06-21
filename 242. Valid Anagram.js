4d012eaff1e456f15ae0e986e41de7fbe4945374
jinho6225 <jinho6225@hotmail.com>
2020-06-20 21:07:57 -0700

    242. valid anagram

--- 242. Valid Anagram.js
+++ 242. Valid Anagram.js
@@ -18,6 +18,30 @@ You may assume the string contains only lowercase alphabets.
  * @param {string} t
  * @return {boolean}
  */
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let map = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!map[s[i]]) {
+      map[s[i]] = 1;
+    } else {
+      map[s[i]]++;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (map[t[i]] === 1) {
+      delete map[t[i]];
+    } else if (map[t[i]] > 1) {
+      map[t[i]]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
 var isAnagram = function (s, t) {
   if (s.length !== t.length) {
     return false;
