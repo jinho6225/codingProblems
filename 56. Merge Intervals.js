5fd38fe82b6169abc7ca11121d4fb5ca196b03a5
jinho6225 <jinho6225@hotmail.com>
2020-07-02 12:25:32 -0700

    56. Merge Intervals

---
+++ 56. Merge Intervals.js
@@ -0,0 +1,39 @@
+/*
+56. Merge Intervals
+Medium
+
+Given a collection of intervals, merge all overlapping intervals.
+Example 1:
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+*/
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i - 1][1] >= intervals[i][1]) {
+      intervals.splice(i, 1);
+      i--;
+    } else {
+      if (
+        intervals[i - 1][1] >= intervals[i][0] &&
+        intervals[i - 1][1] <= intervals[i][1]
+      ) {
+        intervals[i - 1][1] = intervals[i][1];
+        intervals.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return intervals;
+};
