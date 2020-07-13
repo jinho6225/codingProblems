1793b1172358d404570ad94f7d65423cfb433bfe
jinho6225 <jinho6225@hotmail.com>
2020-07-12 23:28:09 -0700

    205. Isomorphic String

---
+++ 205. Isomorphic Strings.js
@@ -0,0 +1,50 @@
+/*
+205. Isomorphic Strings
+Easy
+
+Given two strings s and t, determine if they are isomorphic.
+Two strings are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+Note:
+You may assume both s and t have the same length.
+*/
+
+/*
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false;
+  let obj = {};
+  let obj2 = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!obj[s[i]]) {
+      obj[s[i]] = t[i];
+    } else {
+      if (obj[s[i]] !== t[i]) {
+        return false;
+      }
+    }
+
+    if (!obj2[t[i]]) {
+      obj2[t[i]] = s[i];
+    } else {
+      if (obj2[t[i]] !== s[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
