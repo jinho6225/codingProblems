d1503ce705e7cf7ba77d7f29a3969070d39519e9
jinho6225 <jinho6225@hotmail.com>
2020-07-16 01:11:56 -0700

    572. Subtree of Another Tree

--- 572. Subtree of Another Tree.js
+++ 572. Subtree of Another Tree.js
@@ -1,16 +1,7 @@
 /*
 572. Subtree of Another Tree
-Easy
 
-2305
-
-114
-
-Add to List
-
-Share
 Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
-
 Example 1:
 Given tree s:
 
@@ -28,7 +19,6 @@ Return true, because t has the same structure and node values with a subtree of 
 
 Example 2:
 Given tree s:
-
      3
     / \
    4   5
