b3be3e6401e7d20c3f4e601391b353b46e6dfb41
jinho6225 <jinho6225@hotmail.com>
2020-07-21 00:09:53 -0700

    added another solution

--- 617. Merge Two Binary Trees.js
+++ 617. Merge Two Binary Trees.js
@@ -109,3 +109,35 @@ var tree2 = {
 // }
 
 mergeTrees(tree1, tree2);
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (t1, t2) {
+  function helper(nodeA, nodeB) {
+    if (!nodeA && !nodeB) {
+      return null;
+    }
+    if (!nodeA) {
+      return nodeB;
+    }
+    if (!nodeB) {
+      return nodeA;
+    }
+    let node = new TreeNode(nodeA.val + nodeB.val);
+    node.left = helper(nodeA.left, nodeB.left);
+    node.right = helper(nodeA.right, nodeB.right);
+    return node;
+  }
+  return helper(t1, t2);
+};
