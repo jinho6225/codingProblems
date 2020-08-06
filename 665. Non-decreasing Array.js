8d1fdf8fba7bb6215d2e1e8dbe36c0e9e947cc9d
jinho6225 <jinho6225@hotmail.com>
2020-08-05 17:16:30 -0700

    another solution added

--- 665. Non-decreasing Array.js
+++ 665. Non-decreasing Array.js
@@ -23,6 +23,27 @@ Constraints:
  * @param {number[]} nums
  * @return {boolean}
  */
+
+
+var checkPossibility = function(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length-1; i++) {
+      if (nums[i] > nums[i+1]) {
+          count++
+          if (i === 0) {
+              nums[i] = nums[i+1]
+          } else if (nums[i-1] <= nums[i+1]) {
+              nums[i] = nums[i - 1]
+          } else {
+              nums[i + 1] = nums[i]
+          }
+      }
+      if (count > 1) return false
+  }
+  return true
+};
+
+
 var checkPossibility = function(nums) {
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
