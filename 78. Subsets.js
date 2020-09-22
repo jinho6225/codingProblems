74d0a0f176657a524118419febefbcae4bc0fe91
jinho6225 <jinho6225@hotmail.com>
2020-09-21 17:07:13 -0700

    backtracking - subsets

--- 78. Subsets.js
+++ 78. Subsets.js
@@ -1,56 +1,75 @@
-/*
-78. Subsets
-Medium
+// /*
+// 78. Subsets
+// Medium
 
-Given a set of distinct integers, nums, return all possible subsets (the power set).
-Note: The solution set must not contain duplicate subsets.
-Example:
-Input: nums = [1,2,3]
-Output:
-[
-  [3],
-  [1],
-  [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
-  [1,2],
-  []
-]
-*/
+// Given a set of distinct integers, nums, return all possible subsets (the power set).
+// Note: The solution set must not contain duplicate subsets.
+// Example:
+// Input: nums = [1,2,3]
+// Output:
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+// */
 
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var subsets = function(nums) {
+//     let subsetsArray = [[]]; 
+//     for(const num of nums){ 
+//         for(let array of [...subsetsArray]) 
+//             subsetsArray.push(array.concat(num)); 
+//     }
+//     return subsetsArray;
+// };
+
+// //recursive
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var subsets = function(nums) {
+//     let result = []
+//     const helper = (idx=0, arr=[]) => {
+//         result.push(arr)
+//         for(let i = idx; i < nums.length; i++) {
+//             helper(i+1, arr.concat(nums[i]))
+//         }   
+//     }
+//     helper()
+//     return result
+// };
+
+
+
+
+
+// Input: nums = [1,2,3]
+// Output:
+// [[3],[1],[2],[1,2,3],[1,3],[2,3],[1,2],[]]
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    let subsetsArray = [[]]; 
-    for(const num of nums){ 
-        for(let array of [...subsetsArray]) 
-            subsetsArray.push(array.concat(num)); 
+    let subArr = [[]]
+    for (let i = 0; i < nums.length; i++) {
+        let sub = [...subArr]
+        for (let j = 0; j < sub.length; j++) {
+            subArr.push([...sub[j], nums[i]])
+        }
     }
-    return subsetsArray;
+    console.log(subArr, 'subArr')
+    return subArr;
 };
-
-//recursive
-
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var subsets = function(nums) {
-    let result = []
-    const helper = (idx=0, arr=[]) => {
-        result.push(arr)
-        for(let i = idx; i < nums.length; i++) {
-            helper(i+1, arr.concat(nums[i]))
-        }   
-    }
-    helper()
-    return result
-};
-
-
-
-
-
+console.log(subsets([1,2,3]))
