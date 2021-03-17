251744dd228aace2c1ce966e2019ce2792e8f3c0
jinho6225 <jinho6225@hotmail.com>
2021-03-16 21:35:45 -0700

    more readable, make one more function, refactoring

--- 1041. Robot Bounded In Circle.js
+++ 1041. Robot Bounded In Circle.js
@@ -55,42 +55,50 @@ var isRobotBounded = function(instructions) {
         for (let i = 0; i < 8; i++) {
             order = order.concat(a)
         }
-
-        if (order.includes('G')) {
-            if (!order.includes('L') && !order.includes('R')) {
-                return false
-            } else {
-                let position = [0, 0]
-                let init = [0, 0]
-                let direction = 'N'
-                let map = new Map()
-                for (let i = 0; i < order.length; i++) {
-                    if (order[i] === 'G') {
-                        position = calc(position, direction)
-                        newArr = Array.from(position)
-                        newArr = JSON.stringify(newArr)
-                        if (map.has(newArr)) {
-                            map.set(newArr, map.get(newArr)+1)
-                        } else {
-                            map.set(newArr, 1)
-                        }
-                    } else {
-                        if (order[i] === 'L') {
-                           direction = directWay(direction, order[i])
-                        } else { //order[i] === 'R'
-                           direction = directWay(direction, order[i]) 
-                        }
-                    }
-                }
-
-                let filterArr = Array.from(map.values()).filter((val, i) => val === 1)
-                if (filterArr.length > 0) return false
-                return true
-            }
-        } else {
-            return true
-        }
+		if (order.includes('G')) {
+			if (!order.includes('L') && !order.includes('R')) {
+				return false
+			} else {
+				let i = 0
+				let arr = []
+				while (i < 4) {
+					let result = tracking(order)
+					arr = arr.concat(result)
+					i++
+				}
+				if (arr.includes(1)) return false
+				return true
+			}			
+		} else {
+			return true
+		}
 };
+
+function tracking(order) {
+	let position = [0, 0]
+	let init = [0, 0]
+	let direction = 'N'
+	let map = new Map()
+	for (let i = 0; i < order.length; i++) {
+		if (order[i] === 'G') {
+			position = calc(position, direction)
+			newArr = Array.from(position)
+			newArr = JSON.stringify(newArr)
+			if (map.has(newArr)) {
+				map.set(newArr, map.get(newArr)+1)
+			} else {
+				map.set(newArr, 1)
+			}
+		} else {
+			if (order[i] === 'L') {
+			direction = directWay(direction, order[i])
+			} else { //order[i] === 'R'
+			direction = directWay(direction, order[i]) 
+			}
+		}
+	}
+	return Array.from(map.values())
+}	
 
 
 function directWay(cur, direct) {
@@ -133,6 +141,7 @@ function calc(init, direction) {
     }
     return init
 }
+
 
 /*
 "GRGLGRG"
